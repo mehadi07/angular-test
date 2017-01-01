15cdad44b02535d2c963e1de19369c022f4cba4a
mehadi07 <saimum22@gmail.com>
2017-01-01 18:37:08 -0500

    MoveLog Project

---
+++ MoveLog/img/script.js
@@ -0,0 +1,12 @@
+var text = "Everyone knows how awesome Jonas is!";
+var myName = "Jonas";
+var hits = [];
+
+for (i = 0; i < text.length; i++) {
+    if (text[0] === myName.substring(0,1)) {
+        for (j = i; j <= myName.length + i; j++) {
+            hits.push("text[j]");
+            console.log(hits);
+        }
+    }
+}
